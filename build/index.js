12d9918303cb52266a64763dde6c1de0c1b878f5
Chandru <chandru89new@gmail.com>
2023-03-06 11:13:26 +0530

    Refactor UI.

--- build/index.js
+++ build/index.js
@@ -1,3 +1,14 @@
 const app = Elm.Main.init({
-  node: document.getElementById('app')
-})
+  node: document.getElementById("app"),
+});
+
+document.addEventListener("keydown", (ev) => {
+  const deleteKeyNames = ["backspace", "delete"];
+  const deleteKeyCodes = [8];
+  if (ev.keyCode >= 65 && ev.keyCode <= 90) {
+    app.ports.receiveCharacter.send(ev?.key?.toLowerCase());
+  }
+  if (deleteKeyNames.includes(ev.key) || deleteKeyCodes.includes(ev.keyCode)) {
+    app.ports.receiveBackspace.send(null);
+  }
+});
--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,13 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import API
+import Array exposing (Array)
 import Browser
 import Html as H exposing (Html)
 import Html.Attributes as Attr
 import Html.Events as Ev
+import Json.Decode
+import List.Extra
 import Task
 import Words
 
@@ -15,8 +18,15 @@ main =
         { init = init
         , view = view
         , update = update
-        , subscriptions = \_ -> Sub.none
+        , subscriptions = subscriptions
         }
+
+
+type Letter
+    = NotInWord String
+    | InWord String
+    | InPosition String
+    | Empty String
 
 
 type alias Model =
@@ -24,15 +34,20 @@ type alias Model =
     , exclude : String
     , potentialAnswers : List String
     , positions : String
+    , typedCharacters : Array Letter
     }
+
+
+type alias Position =
+    Int
 
 
 type Msg
     = NoOp
-    | UpdateExcludes String
-    | UpdateIncludes String
-    | UpdatePositions String
     | ComputePotentialAnswers
+    | ReceiveCharacter String
+    | ReceiveBackspace ()
+    | ToggleKey Position
 
 
 init _ =
@@ -40,6 +55,7 @@ init _ =
       , exclude = ""
       , potentialAnswers = []
       , positions = ""
+      , typedCharacters = Array.empty
       }
     , Cmd.none
     )
@@ -47,90 +63,235 @@ init _ =
 
 view : Model -> Html Msg
 view model =
-    H.div [ Attr.class "flex gap-10 flex-col p-10" ]
+    H.div
+        [ Attr.class "grid grid-cols-2 gap-10 p-4"
+        ]
         [ H.div []
-            [ H.input
-                [ Attr.value model.exclude
-                , Ev.onInput UpdateExcludes
-                , Attr.placeholder "Exclusions"
-                , Attr.class
-                    (if String.isEmpty model.exclude then
-                        "empty-input"
-
-                     else
-                        ""
-                    )
+            [ H.div
+                [ Attr.class "grid grid-cols-5 gap-4 w-sm max-w-sm"
                 ]
-                []
+                (List.indexedMap viewLetter <| Array.toList (padEmpty model.typedCharacters))
             ]
-        , H.div []
-            [ H.input
-                [ Attr.value model.include
-                , Ev.onInput UpdateIncludes
-                , Attr.placeholder "Include"
-                , Attr.class
-                    (if String.isEmpty model.include then
-                        "empty-input"
-
-                     else
-                        ""
-                    )
-                ]
-                []
-            ]
-        , H.div []
-            [ H.input
-                [ Attr.value model.positions
-                , Ev.onInput UpdatePositions
-                , Attr.placeholder "Positions"
-                , Attr.class
-                    (if String.isEmpty model.positions then
-                        "empty-input"
-
-                     else
-                        ""
-                    )
-                ]
-                []
-            ]
-        , H.div [] [ H.text <| String.join ", " model.potentialAnswers ]
+        , H.div
+            []
+            (List.map (\wrd -> H.div [] [ H.text wrd ]) model.potentialAnswers)
         ]
 
 
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
             ( model, Cmd.none )
 
-        UpdateExcludes str ->
-            ( { model | exclude = String.trim str }, msgToCmd ComputePotentialAnswers )
-
-        UpdateIncludes str ->
-            ( { model | include = String.trim str }, msgToCmd ComputePotentialAnswers )
-
-        UpdatePositions str ->
-            ( { model | positions = String.trim str }, msgToCmd ComputePotentialAnswers )
-
         ComputePotentialAnswers ->
+            let
+                excludes =
+                    computeExcludesFromModel model.typedCharacters
+
+                includes =
+                    computeIncludesFromModel model.typedCharacters
+
+                positions =
+                    computePositionsFromModel model.typedCharacters
+            in
             ( { model
                 | potentialAnswers =
                     if
-                        String.isEmpty model.exclude
-                            && String.isEmpty model.include
-                            && String.isEmpty model.positions
+                        String.isEmpty excludes
+                            && String.isEmpty includes
+                            && String.isEmpty positions
                     then
                         []
 
                     else
                         Words.words
-                            |> API.exclude model.exclude
-                            |> API.filterByLetters model.include
-                            |> API.filterByPos (String.split "," model.positions)
+                            |> API.exclude excludes
+                            |> API.filterByLetters includes
+                            |> API.filterByPos (String.split "," positions)
               }
             , Cmd.none
             )
+
+        ReceiveCharacter str ->
+            ( { model
+                | typedCharacters = Array.push (NotInWord str) model.typedCharacters
+              }
+            , msgToCmd ComputePotentialAnswers
+            )
+
+        ReceiveBackspace _ ->
+            ( { model
+                | typedCharacters = Array.slice 0 -1 model.typedCharacters
+              }
+            , msgToCmd ComputePotentialAnswers
+            )
+
+        ToggleKey pos ->
+            let
+                itemAtPos =
+                    Array.get pos model.typedCharacters
+
+                replaceItem =
+                    case itemAtPos of
+                        Just item ->
+                            case item of
+                                NotInWord str ->
+                                    InWord str
+
+                                InWord str ->
+                                    InPosition str
+
+                                InPosition str ->
+                                    NotInWord str
+
+                                _ ->
+                                    item
+
+                        -- this should never happen. but have to account for all cases.
+                        Nothing ->
+                            Empty ""
+
+                updatedTypedChars =
+                    Array.set pos replaceItem model.typedCharacters
+            in
+            ( { model | typedCharacters = updatedTypedChars }, msgToCmd ComputePotentialAnswers )
 
 
 msgToCmd : Msg -> Cmd Msg
 msgToCmd msg =
     Task.perform (\_ -> msg) (Task.succeed True)
+
+
+viewLetter : Int -> Letter -> H.Html Msg
+viewLetter idx letter =
+    let
+        renderString : String -> String -> H.Html Msg
+        renderString classes str =
+            H.div
+                [ Attr.class classes
+                , Attr.class "inline-block flex items-center justify-center h-16 w-16 cursor-default"
+                , Attr.id <| "key-" ++ String.fromInt idx
+                , Ev.preventDefaultOn "click" (Json.Decode.succeed ( ToggleKey idx, True ))
+                ]
+                [ H.text <| String.toUpper str ]
+
+        renderNotInWord : String -> H.Html Msg
+        renderNotInWord =
+            renderString "bg-gray-600 text-white"
+
+        renderInWord : String -> H.Html Msg
+        renderInWord =
+            renderString "bg-yellow-400 text-white"
+
+        renderInPosition : String -> H.Html Msg
+        renderInPosition =
+            renderString "bg-green-500 text-white"
+
+        renderEmpty : String -> H.Html Msg
+        renderEmpty =
+            renderString "border border-gray-600"
+    in
+    case letter of
+        NotInWord str ->
+            renderNotInWord str
+
+        InWord str ->
+            renderInWord str
+
+        InPosition str ->
+            renderInPosition str
+
+        Empty str ->
+            renderEmpty str
+
+
+padEmpty : Array Letter -> Array Letter
+padEmpty list =
+    let
+        mod =
+            modBy 5 (Array.length list)
+
+        diff =
+            5 - mod
+    in
+    Array.append list (Array.repeat diff (Empty ""))
+
+
+port receiveCharacter : (String -> msg) -> Sub msg
+
+
+port receiveBackspace : (() -> msg) -> Sub msg
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch
+        [ receiveCharacter ReceiveCharacter
+        , receiveBackspace ReceiveBackspace
+        ]
+
+
+computeIncludesFromModel : Array Letter -> String
+computeIncludesFromModel letters =
+    letters
+        |> Array.foldl
+            (\ltr acc ->
+                case ltr of
+                    InWord str ->
+                        Array.push str acc
+
+                    _ ->
+                        acc
+            )
+            Array.empty
+        |> Array.toList
+        |> List.Extra.unique
+        |> String.concat
+
+
+computeExcludesFromModel : Array Letter -> String
+computeExcludesFromModel letters =
+    letters
+        |> Array.foldl
+            (\ltr acc ->
+                case ltr of
+                    NotInWord str ->
+                        Array.push str acc
+
+                    _ ->
+                        acc
+            )
+            Array.empty
+        |> Array.toList
+        |> List.Extra.unique
+        |> List.filter
+            (\str ->
+                let
+                    stringsInPositionList =
+                        computePositionsFromModel letters
+                in
+                String.contains (String.toUpper str) stringsInPositionList |> not
+            )
+        |> String.concat
+
+
+computePositionsFromModel : Array Letter -> String
+computePositionsFromModel letters =
+    letters
+        |> Array.map
+            (\ltr ->
+                case ltr of
+                    InPosition str ->
+                        String.toUpper str
+
+                    InWord str ->
+                        str
+
+                    _ ->
+                        "-"
+            )
+        |> Array.toList
+        |> List.Extra.greedyGroupsOf 5
+        |> List.map String.concat
+        |> String.join ","
